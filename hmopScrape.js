b72b5ff4ddeea95d4c709d25fa13ef8e85edea7c
mscbr <scibor.maciej@gmail.com>
2020-09-08 21:57:40 +0200

    boilerplate

--- hmopScrape.js
+++ hmopScrape.js
@@ -5,23 +5,23 @@ const { JSDOM } = jsdom;
 
 const url = "https://en.wikipedia.org/wiki/Ministry_of_Health_(Poland)";
 
-const documentRoot = window.document.getElementById("root");
-
-const render = function (template, node) {
-  node.innerHTML = template;
+const getNames = () => {
+  console.log("getNames");
+  axios
+    .get(url)
+    .then((resp) => {
+      const dom = new JSDOM(resp.data);
+      let template = [];
+      for (let i of dom.window.document.querySelectorAll("li > a")) {
+        if (i.href.match(/^\/w\/index.+link=1/)) template.push(i.innerHTML);
+      }
+      return template;
+    })
+    .catch((err) =>
+      err ? console.log(err.message) : console.log("Something went wrong")
+    );
 };
 
-axios
-  .get(url)
-  .then((resp) => {
-    const dom = new JSDOM(resp.data);
-    let template;
-    for (let i of dom.window.document.querySelectorAll("li > a")) {
-      if (i.href.match(/^\/w\/index.+link=1/)) template.push(i.innerHTML);
-    }
-    render(template, documentRoot);
-  })
-  .catch((err) =>
-    err ? console.log(err.message) : console.log("Something went wrong")
-  );
-fetch(url).then((resp) => console.log(resp));
+module.exports = {
+  getNames: () => getNames(),
+};
--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-let http = require("http");
+const express = require("express");
+const http = require("http");
+const logger = require("morgan");
+const reload = require("reload");
 
-const handleRequest = (req, resp) => {
-  resp.writeHead(200, {
-    "Content-Type": "text/plain",
-  });
-  resp.write("<div />");
-  resp.end();
-};
+const hostname = "127.0.0.1";
+const port = 8000;
 
-http.createServer(handleRequest).listen(8000);
+const { getNames } = require("./hmopScrape");
+
+const app = express();
+app.set("port", port);
+app.use(express.json()); // body parser;
+app.use(logger, "dev");
+
+const server = http.createServer((req, res) => {
+  // getNames().then(names => console.log(names));
+  //Set the response HTTP header with HTTP status and Content type
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.write("Hi There!\n");
+  res.end("END OF EXECUTION\n");
+});
+
+//listen for request on port 3000, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
